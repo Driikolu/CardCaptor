e56d83274d0b18042d8e6972d91fe138e645adad
CYM5 <alexandre.monnier35@gmail.com>
2018-03-06 11:30:54 +0100

    Ajout des fonctions propre à la puce, + le reset du hydraNFC

--- handler/hydra_handler.py
+++ handler/hydra_handler.py
@@ -46,3 +46,31 @@ class HydraNFC():
     def field_off(self):
         self.send([0x00, 0x00])
         time.sleep(0.1)
+    def trf7970a_software_init(self):
+        self._serial.write('\x05\x00\x02\x00\x00')
+        self._serial.write('\x83\x83')
+
+    def trf7970a_write_idle(self):
+        self._serial.write('\x05\x00\x02\x00\x00')
+        self._serial.write('\x80\x80')
+    def reset_hydra_nfc(self):
+        """
+        Perform a reset the TRF7970A chip used by the nfc shield.
+        """
+        cmd_lst = [ [ 0x83, 0x83 ],
+                    [ 0x00, 0x21 ],
+                    [ 0x09, 0x00 ],
+                    [ 0x0B, 0x87 ],
+                    [ 0x0B, 0x87 ],
+                    [ 0x8D, ],
+                    [ 0x00, 0x00 ],
+                    [ 0x0D, 0x3E ],
+                    [ 0x14, 0x0F ],
+                    ]
+
+        self.trf7970a_software_init()
+        self.trf7970a_write_idle()
+        for offset in cmd_lst:
+            self.send(offset)
+            time.sleep(0.1)
+
