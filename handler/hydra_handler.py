5f1fc289735e3ead9794a6a1d338df4de6bfe445
CYM5 <alexandre.monnier35@gmail.com>
2018-03-06 11:00:47 +0100

    Première modife du handle pour le hydraNFC, ajout de la fonction send, array_to_str, field_on et field_off

--- handler/hydra_handler.py
+++ handler/hydra_handler.py
@@ -11,7 +11,7 @@ import serial
 
 class HydraNFC():
 
-    def __init__(self, port="COM8", timeout=0,3):
+    def __init__(self, port="/dev/ttyACM0", timeout=0,3):
         self._port=port
         self._timeout=timeout
 
@@ -21,4 +21,28 @@ class HydraNFC():
     def connect(self):
         self._serial=serial.Serial(self._port,timeout=self._timeout)
 
-    
+    def send(self,cmd):
+	'''
+	Send data to the TRF7970A chip
+	
+	0x05 -> TX timer low byte control
+	0x00 -> Chip status control 
+	'''
+	size = chr(len(cmd))
+	self._serial.write('\x05\x00' + size + '\x00\x00') 
+	self._serial.write(self.array_to_str(cmd))
+	
+	
+    def array_to_str(self, cmd):
+	'''
+	Concat the APDU cmd in one string
+	'''
+        return ''.join([chr(c) for c in cmd])
+
+    def field_on(self):
+        self.send([0x00, 0x20])
+        time.sleep(0.1)
+
+    def field_off(self):
+        self.send([0x00, 0x00])
+        time.sleep(0.1)
